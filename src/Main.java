dbe2914363bf80561aa1b4d2461d7ea9c63e0caa
Fabio Murtas <murtasfabio@gmail.com>
2023-02-18 23:42:50 +0100

    modifiche casa

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -6,32 +7,64 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner tastiera = new Scanner(System.in);
-        System.out.println("Ciao segui il menu' per entrare nelle diverse sezioni...");
-        System.out.println("1 Per inserire un ospedale\n" +
-                "2 Per rimuovere un ospedale");
-
+        LinkedList<Ospedale> elencoOspedali = new LinkedList<Ospedale>();
+        System.out.println(
+                " _____  _______  _____  _____ _______ _______      \n" +
+                        "|     | |______ |_____]   |      |    |_____| |    \n" +
+                        "|_____| ______| |       __|__    |    |     | |_____"
+        );
         int scelta;
-        scelta = Integer.parseInt(tastiera.nextLine());
-        while (true) {
-            switch (scelta) {
-                case 1:
-                    LinkedList<Ospedale> elencoOspedali = new LinkedList<Ospedale>();
-                    String testo;
-                    System.out.println("Inserisci il nome dell ospedale : ");
-                    testo = tastiera.nextLine();
-                    Ospedale ospedale = new Ospedale(testo);
-                    elencoOspedali.add(ospedale);
-                    break;
-                case 2:
-                    // altri casi possibili
-                case 3:
-                    // istruzioni da eseguire se l'espressione ha valore2
-                    break;
-                // altri casi possibili
-                default:
-                    // istruzioni da eseguire se l'espressione non corrisponde a nessuno dei casi precedenti
-            }
 
+        while (true) {
+            System.out.println(" Ciao segui il menu' per entrare nelle diverse sezioni...\n" +
+                    "1 Per inserire un ospedale\n" +
+                    "2 Per rimuovere un ospedale\n" +
+                    "3 per visualizzare l' elenco\n" +
+                    "0  per  uscire");
+            try {
+                scelta = Integer.parseInt(tastiera.nextLine());
+
+
+                switch (scelta) {
+                    case 1:
+                        String testo;
+                        System.out.println("Inserisci il nome dell ospedale : ");
+                        testo = tastiera.nextLine();
+                        Ospedale ospedale = new Ospedale(testo);
+                        elencoOspedali.add(ospedale);
+                        break;
+                    case 2:
+                        do {
+                            System.out.println("1 Per rimuovere l'ultimo\n" +
+                                    "2 Per rimuovere il primo\n" +
+                                    "0 Per tornare al menu' precedente");
+                            scelta = Integer.parseInt(tastiera.nextLine());
+                            if (scelta == 1) {
+                                elencoOspedali.removeLast();
+                            }
+                            if (scelta == 2) {
+                                elencoOspedali.removeFirst();
+                            }
+                        } while (scelta != 0);
+
+                    case 3:
+                        System.out.println("Elenco ospedali creati: \n");
+                        for (Ospedale i : elencoOspedali) {
+                            System.out.println(i.getNome());
+                        }
+                        break;
+                    // altri casi possibili
+                    case 0:
+                        System.exit(0);
+                        break;
+                    default:
+                        // istruzioni da eseguire se l'espressione non corrisponde a nessuno dei casi precedenti
+                }
+
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage() + "\nNon hai inserito un numero valido, \n" +
+                        "oppure hai inserito una lettera, ritenta!..");
+            }
         }
     }
 }
